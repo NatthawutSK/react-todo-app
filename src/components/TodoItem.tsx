27bbe52f6a1c42f4e9f4aa05e49924870638747a
FixFire <89625074+NatthawutSK@users.noreply.github.com>
2023-05-29 13:27:38 +0700

    opal

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
+import "./TodoItem.css";
 
 type Props = {
   todo: string;
-  onEdit: (tdo: any) => void;
+  isCompleted: boolean;
+  onEdit: (newTodo: string) => void;
   onDelete: () => void;
+  onToggle: () => void;
 };
 
-export default function TodoItem({ todo, onEdit, onDelete }: Props) {
+export default function TodoItem({
+  todo,
+  isCompleted,
+  onEdit,
+  onDelete,
+  onToggle,
+}: Props) {
   const [editMode, setEditMode] = useState(false);
   const [editedTodo, setEditedTodo] = useState(todo);
 
@@ -19,26 +28,36 @@ export default function TodoItem({ todo, onEdit, onDelete }: Props) {
 
   return (
     <>
-      <div className="relative w-full max-w-[24rem]">
-        <div className=" box h-15 w-50 p-4 rounded-md bg-white">
+      <div className="relative w-full  pt-6 ">
+        <div className=" box h-15 p-4 rounded-md bg-white">
+          <label>
+            <input
+              className="flex w-5 h-5 accent-pink-500"
+              type="checkbox"
+              checked={isCompleted}
+              onChange={onToggle}
+            />
+          </label>
           {editMode ? (
             <input
+              className="w-5/6 bg-red-200"
               type="text"
               value={editedTodo}
               onChange={(e) => setEditedTodo(e.target.value)}
             />
           ) : (
-            <p>{editedTodo}</p>
+            <p className={isCompleted ? "completed" : ""}>{todo}</p>
           )}
+
           <button
             onClick={handleEdit}
-            className="!absolute right-20 top-1 rounded bg-red-200"
+            className="!absolute right-20 top-7 rounded bg-red-200 "
           >
             {editMode ? "Save" : "Edit"}
           </button>
           <button
             onClick={onDelete}
-            className="!absolute right-1 top-1 rounded bg-red-200"
+            className="!absolute right-1 top-7 rounded bg-red-200"
           >
             Del
           </button>
--- src/pages/TodoPage.tsx
+++ src/pages/TodoPage.tsx
@@ -10,19 +10,21 @@ import TodoItem from "@/components/TodoItem";
 type Props = {};
 
 export default function page1({}: Props) {
-  const [todos, setTodos] = useState<string[]>([]);
+  const [todos, setTodos] = useState<{ todo: string; isCompleted: boolean }[]>(
+    []
+  );
   const [newTodo, setNewTodo] = useState<string>("");
 
   const handleAddTodo = () => {
     if (newTodo.trim() !== "") {
-      setTodos([...todos, newTodo]);
+      setTodos([...todos, { todo: newTodo, isCompleted: false }]);
       setNewTodo("");
     }
   };
 
   const handleEditTodo = (index: number, newTodo: string) => {
     const updatedTodos = [...todos];
-    updatedTodos[index] = newTodo;
+    updatedTodos[index].todo = newTodo;
     setTodos(updatedTodos);
   };
 
@@ -30,35 +32,43 @@ export default function page1({}: Props) {
     const updatedTodos = todos.filter((_, i) => i !== index);
     setTodos(updatedTodos);
   };
+
+  const handleToggleTodo = (index: number) => {
+    const updatedTodos = [...todos];
+    updatedTodos[index].isCompleted = !updatedTodos[index].isCompleted;
+    setTodos(updatedTodos);
+  };
   return (
     <>
-      {/* <h2>{inputVal}</h2> */}
-      {/* <h2>{todo}</h2> */}
-      <h1>Todo App</h1>
-      <br />
-      <div className="container">
-        <Input
-          value={newTodo}
-          onChange={(e) => setNewTodo(e.target.value)}
-          className="bg-stone-50"
-        />
-
-        <Button className="btnAdd bg-red-400" onClick={handleAddTodo}>
-          Add
-          <BookmarkIcon strokeWidth={2} className="h-5 w-5 text-white" />
-        </Button>
-      </div>
-
-      <ul>
-        {todos.map((todo, index) => (
-          <TodoItem
-            key={index}
-            todo={todo}
-            onEdit={(newTodo) => handleEditTodo(index, newTodo)}
-            onDelete={() => handleDeleteTodo(index)}
+      <div className="container mx-auto">
+        <h1>Todo App</h1>
+        <br />
+        <div className="container1">
+          <Input
+            value={newTodo}
+            onChange={(e) => setNewTodo(e.target.value)}
+            className="bg-stone-50 h-10"
           />
-        ))}
-      </ul>
+
+          <Button className="btnAdd bg-red-400" onClick={handleAddTodo}>
+            Add
+            <BookmarkIcon strokeWidth={2} className="h-5 w-5 text-white" />
+          </Button>
+        </div>
+
+        <ul>
+          {todos.map((todoItem, index) => (
+            <TodoItem
+              key={index}
+              todo={todoItem.todo}
+              isCompleted={todoItem.isCompleted}
+              onEdit={(newTodo) => handleEditTodo(index, newTodo)}
+              onDelete={() => handleDeleteTodo(index)}
+              onToggle={() => handleToggleTodo(index)}
+            />
+          ))}
+        </ul>
+      </div>
     </>
   );
 }
