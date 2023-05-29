dd9de0ba043cc6297a830fea7d3be9d46024125e
FixFire <89625074+NatthawutSK@users.noreply.github.com>
2023-05-29 12:24:30 +0700

    aaaaa

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 type Props = {
-  content: any;
+  todo: string;
+  onEdit: (tdo: any) => void;
+  onDelete: () => void;
 };
 
-export default function TodoItem({ content }: Props) {
-  return <div> - {content}</div>;
+export default function TodoItem({ todo, onEdit, onDelete }: Props) {
+  const [editMode, setEditMode] = useState(false);
+  const [editedTodo, setEditedTodo] = useState(todo);
+
+  const handleEdit = () => {
+    if (editMode) {
+      onEdit(editedTodo);
+    }
+    setEditMode(!editMode);
+  };
+
+  return (
+    <div>
+      {editMode ? (
+        <input
+          type="text"
+          value={editedTodo}
+          onChange={(e) => setEditedTodo(e.target.value)}
+        />
+      ) : (
+        <p>{editedTodo}</p>
+      )}
+      <button onClick={handleEdit}>{editMode ? "Save" : "Edit"}</button>
+      <button onClick={onDelete}>Delete</button>
+    </div>
+  );
 }
--- src/pages/TodoPage.tsx
+++ src/pages/TodoPage.tsx
@@ -1,55 +1,64 @@
 /* eslint-disable @typescript-eslint/ban-types */
 
 import { useState } from "react";
-import { Button,Input } from "@material-tailwind/react";
-import {BookmarkIcon} from "@heroicons/react/24/outline";
+import { Button, Input } from "@material-tailwind/react";
+import { BookmarkIcon } from "@heroicons/react/24/outline";
 
-import '../pages/TodoPage.css'
+import "../pages/TodoPage.css";
 import TodoItem from "@/components/TodoItem";
 /* eslint-disable no-empty-pattern */
 type Props = {};
 
 export default function page1({}: Props) {
-  const [newTodo, setnewTodo] = useState("");
-  const [inputVal, setinputVal] = useState("");
-  const [todo, settodo] = useState([]);
+  const [todos, setTodos] = useState<string[]>([]);
+  const [newTodo, setNewTodo] = useState<string>("");
 
-  const changeName = () => {
-    const newArray: any = [...todo, inputVal];
-
-    // Update the array state using setMyArray
-    settodo(newArray);
-    setnewTodo(inputVal);
-    setinputVal("");
+  const handleAddTodo = () => {
+    if (newTodo.trim() !== "") {
+      setTodos([...todos, newTodo]);
+      setNewTodo("");
+    }
   };
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(event.target.value);
-    setinputVal(event.target.value);
+  const handleEditTodo = (index: number, newTodo: string) => {
+    const updatedTodos = [...todos];
+    updatedTodos[index] = newTodo;
+    setTodos(updatedTodos);
+  };
+
+  const handleDeleteTodo = (index: number) => {
+    const updatedTodos = todos.filter((_, i) => i !== index);
+    setTodos(updatedTodos);
   };
   return (
     <>
-      
       {/* <h2>{inputVal}</h2> */}
       {/* <h2>{todo}</h2> */}
-      <h1>Todo App</h1><br />
+      <h1>Todo App</h1>
+      <br />
       <div className="container">
-        <Input className="bg-stone-50"
-        onChange={handleInputChange}/>
+        <Input
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+          className="bg-stone-50"
+        />
 
-        <Button className="btnAdd bg-red-400"  onClick={changeName}>
-        Add<BookmarkIcon strokeWidth={2} className="h-5 w-5 text-white" /> 
-      </Button>
-     
+        <Button className="btnAdd bg-red-400" onClick={handleAddTodo}>
+          Add
+          <BookmarkIcon strokeWidth={2} className="h-5 w-5 text-white" />
+        </Button>
       </div>
 
       <ul>
-        {todo.map((item, index) => (
-          <TodoItem key={index} content={item} />
+        {todos.map((todo, index) => (
+          <TodoItem
+            key={index}
+            todo={todo}
+            onEdit={(newTodo) => handleEditTodo(index, newTodo)}
+            onDelete={() => handleDeleteTodo(index)}
+          />
         ))}
       </ul>
-     
-     
     </>
   );
 }
